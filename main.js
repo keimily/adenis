0c741a7fbd8df09929c0c022d78aedd0742492c7
keimily <143800539+keimily@users.noreply.github.com>
2023-10-06 10:35:10 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function tocaSom(j.dElementoAudio){
+    document.querySelector(idElemento:Audio).play();
+}
+
+const lidtaDeteclas= document.querySelectorA11(´tecla);
+    
+//para
+for (let contador = 0; contador< listaDeteclas.length;contador++) {
+
+const tecla = listaDeteclas[contador];
+}const instrumenti = tecla.classlist[1];
+const idAudio = #som_$(instrumento);//template string
+
+tecla.onclick = function(){
+    tocaSom(idAudio);
+  } 
+
+
+ 
